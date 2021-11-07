b8c71dce46c7c89ec087ef00ebcc5722e56f3b86
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-07 15:54:48 +0900

    change fragment

--- src/pages/index.js
+++ src/pages/index.js
@@ -155,13 +155,7 @@ export const query = graphql`
     file(relativePath: { eq: "hero.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
-          base64
-          aspectRatio
-          src
-          srcSet
-          srcSetWebp
-          srcWebp
-          sizes
+          ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
