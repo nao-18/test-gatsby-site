63634f517533d27f5e70e1ce0e801072617e3123
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-07 20:20:53 +0900

    fixed 404 page css

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,8 @@ import Layout from '../components/layout';
 
 export default () => (
   <Layout>
-    <h1>お探しのページが見つかりませんでした</h1>
+    <h1 style={{ padding: '20vh 0', textAlign: 'center' }}>
+      お探しのページが見つかりませんでした
+    </h1>
   </Layout>
 );
