b9c712291eb5f27add69ea6b6751a0d7727e5d4e
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-07 18:51:20 +0900

    add layout.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
+import './layout.css';
+
 export default ({ children }) => (
   <div>
     <Header />
