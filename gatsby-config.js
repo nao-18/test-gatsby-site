2fbc9490721f4945a8ee8e728a78d165effbeac6
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-07 14:37:32 +0900

    add setting for gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "demoSite",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'demoSite',
   },
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
